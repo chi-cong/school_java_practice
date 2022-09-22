986b7cf181dd9ef5d0ed818edcc95badfd215632
Chi Cong <chicong422002@gmail.com>
2022-09-23 00:19:59 +0700

    doing exercise 4.4

--- work_shop_4.4/src/Student_Console/IStudent.java
+++ work_shop_4.4/src/Student_Console/IStudent.java
@@ -7,8 +7,8 @@ public interface IStudent {
 	boolean delStudent(String id);
 	Connection getCon();
 	ArrayList<Student> getListStudent();
-	ArrayList<Student> getStudent_byClass(String ClassID, boolean gender);
-	ArrayList<Student> getStudent_byClass_gender(String ClassID, boolean gender);
+	ArrayList<Student> getStudent_byClass(String ClassID);
+	ArrayList<Student> getStudent_byClass_gender(String ClassID, String gender);
 	Student getStudent_byId(String id);
 	boolean insertStudent(String id, String name, String gender, String classId, String address, Double mark );
 	boolean updateStudent(String id, String name, String gender, String classId, String address, Double mark );
--- work_shop_4.4/src/Student_Console/Process_student.java
+++ work_shop_4.4/src/Student_Console/Process_student.java
@@ -1,56 +1,153 @@
 package Student_Console;
 
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
 import java.util.ArrayList;
+
+import com.mysql.cj.protocol.Resultset;
+
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class Process_student implements IStudent {
 
-	@Override
 	public boolean delStudent(String id) {
-		// TODO Auto-generated method stub
+		                 
 		return false;
 	}
 
-	@Override
 	public Connection getCon() {
-		// TODO Auto-generated method stub
+		String url = "jdbc:mysql://localhost:3306/db_student";
+		String userName = "root";
+		String password = "Dat422002";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection cn = DriverManager.getConnection(url, userName, password);
+			return cn;
+		} catch ( ClassNotFoundException | SQLException e) {
+			System.out.print(e);
+		}
 		return null;
 	}
 
-	@Override
 	public ArrayList<Student> getListStudent() {
-		// TODO Auto-generated method stub
+		Connection conn = getCon();
+		String query = "Select * from tbStudent";
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				Student student = new Student();
+				student.setId(resultSet.getString("id"));
+				student.setName("name");
+				student.setClassID(resultSet.getString("idClass"));
+				student.setGender(resultSet.getString("gender"));
+				student.setAddress(resultSet.getString("address"));
+				student.setMark(resultSet.getDouble("mark"));
+				studentList.add(student);
+				return studentList;
+			}
+		} catch ( SQLException e) {
+			System.out.print(e);
+		}
 		return null;
 	}
 
-	@Override
-	public ArrayList<Student> getStudent_byClass(String ClassID, boolean gender) {
-		// TODO Auto-generated method stub
+	public ArrayList<Student> getStudent_byClass(String ClassID) {
+		Connection conn = getCon();
+		String query = "Select * from tbStudent where idClass = '" + ClassID +"'";
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				Student student = new Student();
+				student.setId(resultSet.getString("id"));
+				student.setName("name");
+				student.setClassID(resultSet.getString("idClass"));
+				student.setGender(resultSet.getString("gender"));
+				student.setAddress(resultSet.getString("address"));
+				student.setMark(resultSet.getDouble("mark"));
+				studentList.add(student);
+				return studentList;
+			}
+		} catch ( SQLException e) {
+			System.out.print(e);
+		}
 		return null;
 	}
 
-	@Override
-	public ArrayList<Student> getStudent_byClass_gender(String ClassID, boolean gender) {
-		// TODO Auto-generated method stub
+	public ArrayList<Student> getStudent_byClass_gender(String ClassID, String gender) {
+		
+		Connection conn = getCon();
+		String query = "Select * from tbStudent where idClass = '" + ClassID + "', gender = '" + gender + "'";
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				Student student = new Student();
+				student.setId(resultSet.getString("id"));
+				student.setName("name");
+				student.setClassID(resultSet.getString("idClass"));
+				student.setGender(resultSet.getString("gender"));
+				student.setAddress(resultSet.getString("address"));
+				student.setMark(resultSet.getDouble("mark"));
+				studentList.add(student);
+				return studentList;
+			}
+		} catch ( SQLException e) {
+			System.out.print(e);
+		}
 		return null;
 	}
 
-	@Override
 	public Student getStudent_byId(String id) {
-		// TODO Auto-generated method stub
+		
+		Connection conn = getCon();
+		String query = "Select * from tbStudent where id = '" + id + "'";
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				Student student = new Student();
+				student.setId(resultSet.getString("id"));
+				student.setName("name");
+				student.setClassID(resultSet.getString("idClass"));
+				student.setGender(resultSet.getString("gender"));
+				student.setAddress(resultSet.getString("address"));
+				student.setMark(resultSet.getDouble("mark"));
+				return student;
+			}
+		} catch ( SQLException e) {
+			System.out.print(e);
+		}
 		return null;
 	}
 
-	@Override
 	public boolean insertStudent(String id, String name, String gender, String classId, String address, Double mark) {
-		// TODO Auto-generated method stub
+		
+		Connection conn = getCon();
+		String insertPrefix = "insert into tbStudent (id, name, gender, idClass, address, mark)";
+		String insertedValue = "value ('" + id + "', '" + name + "', '" + gender + "', '" + classId + "', '" + address + "', " + mark;
+		ArrayList<Student> studentList = new ArrayList<Student>();
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+		} catch ( SQLException e) {
+			System.out.print(e);
+		}
 		return false;
 	}
 
-	@Override
 	public boolean updateStudent(String id, String name, String gender, String classId, String address, Double mark) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
+	
+	
 
 }
