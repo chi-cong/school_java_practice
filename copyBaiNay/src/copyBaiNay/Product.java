e601e82365472a23cef7c795bf975337b431e678
Chi Cong <chicong422002@gmail.com>
2022-10-05 16:14:59 +0700

    bai 2

--- copyBaiNay/src/copyBaiNay/Product.java
+++ copyBaiNay/src/copyBaiNay/Product.java
@@ -1,5 +1,30 @@
 package copyBaiNay;
 
 public class Product {
+	String ID;
+	String name;
+	public Product() {
+		
+	}
+	
+	public Product(String ID, String name) {
+		this.ID = ID;
+		this.name = name;
+	}
 
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
